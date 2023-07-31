b3585cb2935b9de074c7dd87c1f60642d913d0b6
Sreeram V <sreeramvijayan007@gmail.com>
2023-07-31 16:35:49 +0530

    changes

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { elementAt, findIndex } from 'rxjs';
 
 @Component({
   selector: 'app-question',
@@ -9,14 +10,43 @@ export class QuestionComponent implements OnInit {
   @Input() question: any;
   answer: string = '';
   showIf: boolean = false;
+  sub: any;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  changeVal(value: any) {
-    if (value === 'Apple') this.showIf = true
-    else if (value === 'Atari' || value === 'Microsoft') this.showIf = false;
+  changeVal(value: any , i : any) {
+    this.question.forEach((element: any) => {
+        if(element.subQue !== undefined) {
+          this.sub =  element.subQue.filter((sub:any) => sub.showIf === value);
+          console.log(this.sub);
+        }
+    })
+
+
+
+
+
+
+
+
+
+
+    // if (value === 'Apple') this.showIf = true
+    // // else if (value === 'Atari' || value === 'Microsoft') this.showIf = false;
+    // this.question.forEach((element: any) => {
+    //   if(element.subQue !== undefined) {
+        
+    //     // this.sub =  element.subQue.filter((sub:any) => sub.showIf === value);
+    //     console.log( this.sub)
+    //     // element.subQue.forEach((subelement:any) => {
+    //       // console.log(subelement.showIf ,value );
+    //       // if(subelement.showIf === value) this.showIf = true
+    //       // else this.showIf = false;
+    //     // })
+    //   }
+    // });
   }
 }
--- src/app/quiz-form/quiz-form.component.ts
+++ src/app/quiz-form/quiz-form.component.ts
@@ -7,13 +7,42 @@ import { QuizDataService } from '../quiz-data.service';
   styleUrls: ['./quiz-form.component.css']
 })
 export class QuizFormComponent implements OnInit {
-  quizQuestions: any = [];
+  quizQuestions: any = [
+    {
+      "queType": "Main",
+      "type": "dropdown",
+      "question": "Which company was established in by Bill Gates ?",
+      "options": ["Apple", "Microsoft", "Atari"],
+      "subQue": [
+        {
+          "queType": "Sub",
+          "showIf": "Microsoft",
+          "type": "dropdown",
+          "question": "What is the most common way to iterate through an array ?",
+          "options": ["For loop", "If Statements"]
+        },
+        {
+          "queType": "Sub",
+          "showIf": "Apple",
+          "type": "textinput",
+          "question": "Which type of code is represented in 0 and 1 ?",
+          "options": []
+        }
+      ]
+    },
+    {
+      "queType": "Main",
+      "type": "dropdown",
+      "question": "What is use for CSS ?",
+      "options": ["Styling websites", "JS logic", "Database"]
+    }
+  ]
 
   constructor(private quizDataService: QuizDataService) { }
 
   ngOnInit() {
-    this.quizDataService.getQuizData().subscribe((data: any) => {
-      this.quizQuestions = data;
-    });
+    // this.quizDataService.getQuizData().subscribe((data: any) => {
+    //   this.quizQuestions = data;
+    // });
   }
 }
